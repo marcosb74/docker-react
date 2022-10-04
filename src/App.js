3ef128f32ec700c1da442851ccfe1926de118bf8
as-takeba <markos.de.vl@gmail.com>
2022-10-04 19:20:41 -0300

    new b

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hey There!!! </p>
+        <p>Hey There!!! Welcome !!! </p>
         <a
           className="App-link"
           href="https://reactjs.org"
