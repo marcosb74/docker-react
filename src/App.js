f013f9d498d2dd021440e6cd96058291f9a6f435
marcosb74 <markos.de.vl@gmail.com>
2022-10-13 02:22:54 -0300

    addition

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hey There!!! Welcome !!! To!!! The nwest app!! </p>
+        <p>Hey There!!! Welcome !!! To!!! The nwest app!! To</p>
         <a
           className="App-link"
           href="https://reactjs.org"
